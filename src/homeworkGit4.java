b7c113b9a2fa7999ee6af8c6cb89a193feaf1785
Wicked <bekarov@gmail.com>
2022-06-14 17:26:44 +0200

    Homework commit amending

--- src/homeworkGit4.java
+++ src/homeworkGit4.java
@@ -1,7 +1,8 @@
 public class homeworkGit4 {
     public static void main(String[] args) {
-        System.out.println("Homework commit 1");
-        System.out.println("Homework commit 2");
+        System.out.println("Homework commit amending");
+        System.out.println("Homework commit amending 2");
         System.out.println("Homework commit 3");
+        System.out.println("Homework IntelliJ commit 3");
     }
 }
