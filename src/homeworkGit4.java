150868d6d764d0574a71fbca6111ffea903c44bd
Wicked <bekarov@gmail.com>
2022-06-15 19:17:58 +0200

    add new variable

--- src/homeworkGit4.java
+++ src/homeworkGit4.java
@@ -2,6 +2,7 @@ public class homeworkGit4 {
     public static void main(String[] args) {
         System.out.println("Homework commit 1");
         System.out.println("Homework commit 2");
-        System.out.println("Commit 2");
+        System.out.println("Commit 1");
+        int a = 6;
     }
 }
