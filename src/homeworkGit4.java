73f34f03d9f7516444eaf3050073a97d3c8a04ed
Wicked <bekarov@gmail.com>
2022-06-15 18:50:23 +0200

    Revert "Reverting"
    This reverts commit 782a09e051ce77805a47b0319caf55b9ecdb6071.

--- src/homeworkGit4.java
+++ src/homeworkGit4.java
@@ -2,6 +2,5 @@ public class homeworkGit4 {
     public static void main(String[] args) {
         System.out.println("Homework commit 1");
         System.out.println("Homework commit 2");
-        System.out.println("homework reverting");
     }
 }
