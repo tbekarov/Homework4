6180235895346b6dbf387497de8a8f4614a4a28b
Wicked <bekarov@gmail.com>
2022-06-15 18:53:17 +0200

    Homework commit 1

--- src/homeworkGit4.java
+++ src/homeworkGit4.java
@@ -2,5 +2,6 @@ public class homeworkGit4 {
     public static void main(String[] args) {
         System.out.println("Homework commit 1");
         System.out.println("Homework commit 2");
+        System.out.println("Commit 2");
     }
 }
