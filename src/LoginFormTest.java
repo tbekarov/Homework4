663d58214dc242366a9f1550bdb9407ab034da4f
Wicked <bekarov@gmail.com>
2022-06-15 19:35:34 +0200

    Added new changes

--- src/LoginFormTest.java
+++ src/LoginFormTest.java
@@ -1,5 +1,6 @@
 public class LoginFormTest {
     public static void main(String[] args) {
         System.out.println("Automation test for login form");
+        System.out.println("New changes");
     }
 }
